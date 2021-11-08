71f64076d6c86d8858a88d25e77b767d62bbc36b
asraful <iasraful107@gmail.com>
2021-11-08 17:49:54 +0600

    hello

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
+const{ MongoClient } = require('mongodb');
 
 
 const port = process.env.PORT || 5000;
